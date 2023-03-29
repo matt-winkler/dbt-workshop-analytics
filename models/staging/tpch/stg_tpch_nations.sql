944c3a9f671c58fa38bd73424af9909d8c0b284b
Luis Leon <98919783+luisleon90@users.noreply.github.com>
2023-03-29 16:56:57 -0400

    Update stg_tpch_nations.sql
    removing changes after successfully trigger prod deployment after merging to main

--- models/staging/tpch/stg_tpch_nations.sql
+++ models/staging/tpch/stg_tpch_nations.sql
@@ -11,8 +11,7 @@ renamed as (
         n_nationkey as nation_key,
         n_name as name,
         n_regionkey as region_key,
-        n_comment as comment,
-        1 as new_col
+        n_comment as comment
 
     from source
 
