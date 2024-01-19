c76562ba03705dfb531501ab678e92d524e66f11
Alex <alex.dinsmoor@dbtlabs.com>
2024-01-19 04:29:19 +0000

    add dim_customer mesh logic (access, contracts, versions). add fct_order_items as public model too. update project name from 'tpch' to 'analytics'.

--- models/marts/core/dim_customers_v2.sql
+++ models/marts/core/dim_customers_v2.sql
@@ -19,6 +19,10 @@ region as (
     select * from {{ ref('stg_tpch_regions') }}
 
 ),
+customer_flags as (
+    select * from {{ ref('customer_flags') }}
+),
+
 final as (
     select 
         customer.customer_key,
@@ -30,13 +34,19 @@ final as (
         region.name as region,
         customer.phone_number,
         customer.account_balance,
-        customer.market_segment
+        customer.market_segment,
+        customer_flags.lifetime_value,
+        customer_flags.is_high_value,
+        customer_flags.is_mid_value,
+        customer_flags.is_low_value
     from
         customer
         inner join nation
             on customer.nation_key = nation.nation_key
         inner join region
             on nation.region_key = region.region_key
+        left join customer_flags
+            on customer.customer_key = customer_flags.customer_key
 )
 select 
     *
